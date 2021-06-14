088e4352835ab7689abd678fa951582b2e19e087
zenithshrestha37 <zenithshrestha37@gmail.com>
2021-06-14 13:30:37 -0500

    Added calculator logic

--- calc.js
+++ calc.js
@@ -15,6 +15,31 @@ reader.question("What would you like to calculate?", function(input){
 	num2 = Number(tokens[2]);
 
 
+    
+	console.log('mathSymbol', mathSymbol);
+            console.log('num1', num1);
+            console.log('num2', num2);
+if (mathSymbol == '+'){
+	console.log(num1 + num2);
+}
+
+if (mathSymbol == '-'){
+	console.log(num1 - num2);
+}
+
+if (mathSymbol == '*'){
+	console.log(num1 * num2);
+}
+if (mathSymbol == '/'){
+	console.log(num1 / num2);
+}
+if (mathSymbol == '^'){
+	console.log(Math.sqrt(num1));
+	console.log(Math.sqrt(num2));
+	
+}
+
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 
